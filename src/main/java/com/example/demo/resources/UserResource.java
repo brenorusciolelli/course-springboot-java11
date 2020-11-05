0e9c1f3addcb34596bd526150470db86e4b02d6e
brenorusciolelli <brenorusciolelli@gmail.com>
2020-11-05 14:00:29 -0300

    User entity and resource

---
+++ src/main/java/com/example/demo/resources/UserResource.java
@@ -0,0 +1,20 @@
+package com.example.demo.resources;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.example.demo.entities.User;
+
+@RestController
+@RequestMapping(value = "/users")
+public class UserResource {
+	
+	@GetMapping
+	public ResponseEntity<User> findAll() {
+		User u = new User(1L, "Breno", "brenorusciolelli@gmail.com","810715553", "123456");
+		return ResponseEntity.ok().body(u);
+	}
+
+}
